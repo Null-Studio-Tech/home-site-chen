7fade6f955e0c658a5acafdea2c5eaf5f51e4635
董志刚 <dongzhigang_bit@126.com>
2023-06-12 09:00:59 +0800

    ci: 配置github-page脚本

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export'
+}
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+nextConfig.assetPrefix = assetPrefix
+nextConfig.basePath = basePath
+
 
 module.exports = nextConfig
